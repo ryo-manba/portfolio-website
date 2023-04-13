983f7d4accc587e12d6c6f9082f32a0f58c4cb42
Ryo <matsukawa.ryo42@gmail.com>
2023-04-13 21:00:35 +0900

    add: srcディレクトリを作成した

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Header } from '../components/Header/Header';
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div> Home</div>
+      </main>
+    </>
+  );
+}
