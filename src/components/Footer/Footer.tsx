17f163b3a3b13f2d898ec9041666e957040cb2e3
Ryo <matsukawa.ryo42@gmail.com>
2023-04-15 11:37:54 +0900

    fixes #2 フッターを作成する

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,39 @@
+import { memo } from 'react';
+import {
+  TiSocialLinkedin,
+  TiSocialGithub,
+  TiSocialTwitter,
+} from 'react-icons/ti';
+
+export const Footer = memo(function Footer() {
+  return (
+    <>
+      <footer className="bg-[#D9D9D9] h-32 flex flex-col items-center justify-center">
+        <div className="flex space-x-4 mb-2">
+          <a
+            href="https://github.com/ryo-manba"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <TiSocialGithub className="text-4xl" />
+          </a>
+          <a
+            href="https://twitter.com/ryo_manba"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <TiSocialTwitter className="text-4xl" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/ryo-matsukawa-ba5276214/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <TiSocialLinkedin className="text-4xl" />
+          </a>
+        </div>
+        <p className="text-sm">&copy; 2023 ryo-manba</p>
+      </footer>
+    </>
+  );
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
 import { Header } from '../components/Header/Header';
-
+import { Footer } from '../components/Footer/Footer';
 export default function Home() {
   return (
     <>
-      <Header />
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-grow"></main>
+        <Footer />
+      </div>
     </>
   );
 }
