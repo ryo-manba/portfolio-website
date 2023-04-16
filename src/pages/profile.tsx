92081d71b27b73fed00f0bef73a3e62fecd6d9e6
Ryo <matsukawa.ryo42@gmail.com>
2023-04-16 19:47:38 +0900

    close #6 Profileページを作成する

---
+++ src/pages/profile.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+
+const PROFILE_IMAGE_PATH = '/images/profile-image.png';
+
+const Profile = () => {
+  return (
+    <div className="mt-14">
+      <h1 className="text-center text-4xl font-bold">Profile</h1>
+      <section className="flex items-center justify-center mt-12 ml-16 space-x-6">
+        <Image
+          src={PROFILE_IMAGE_PATH}
+          alt="Profile Image"
+          className="w-32 h-32 mb-2 rounded-full border-4 border-white"
+          width={250}
+          height={250}
+        />
+        <div>
+          <h2 className="text-[26px] mb-2">
+            <ruby>
+              松川 陵<rt className="text-gray-400">Matsukawa Ryo</rt>
+            </ruby>
+          </h2>
+          <p>
+            42 Tokyo / Wine Sommelier
+            <br />
+            サイボウズの23卒エンジニア
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Profile;
