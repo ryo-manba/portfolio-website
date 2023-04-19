3fa0295335a18b9db46c4579aac327384db4a76a
Ryo <matsukawa.ryo42@gmail.com>
2023-04-19 21:01:49 +0900

    add: タグコンポーネントを追加した

---
+++ src/components/Tag/Tag.tsx
@@ -0,0 +1,12 @@
+import { memo } from 'react';
+type Props = {
+  label: string;
+};
+
+export const Tag = memo(function Tag({ label }: Props) {
+  return (
+    <span className="m-1 inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-[#D9D9D9] text-[#4B4B4B]">
+      {label}
+    </span>
+  );
+});
