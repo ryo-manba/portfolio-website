6c675641ed96e578b6891ce753ca513c3b45f332
Ryo <matsukawa.ryo42@gmail.com>
2023-04-22 18:04:13 +0900

    add: hatenaブログの記事を取得するapi

---
+++ src/hooks/useHatena.ts
@@ -0,0 +1,62 @@
+import useSWR from 'swr';
+import axios from 'axios';
+import { useMemo } from 'react';
+
+const fetcher = (url: string) => axios.get(url).then((res) => res.data);
+
+// TODO: 型定義を別ファイルにまとめる
+type HatenaPost = {
+  name: string;
+  domain: string;
+  favicon: string;
+  title: string;
+  url: string;
+  createdAt: string;
+};
+
+type User = {
+  name: string;
+};
+
+type Contents = Content[];
+
+type Link = {
+  href: string;
+};
+
+type Content = {
+  day: string;
+  title: string;
+  href: string;
+};
+
+const PROXY_PATH = '/api/hatena';
+export const useHatena = () => {
+  const { data, error, isLoading } = useSWR(PROXY_PATH, fetcher);
+
+  console.log('data', data);
+
+  const contents: Contents = useMemo(() => {
+    return data || [];
+  }, [data]);
+
+  const posts: HatenaPost[] = useMemo(() => {
+    return contents.map((content) => {
+      const post: HatenaPost = {
+        name: 'taro',
+        domain: 'hatenablog.com',
+        favicon: '/images/hatenablog-logo.svg',
+        title: content.title,
+        url: content.href,
+        createdAt: content.day,
+      };
+      return post;
+    });
+  }, [contents]);
+
+  return {
+    posts,
+    isLoading,
+    error,
+  };
+};
---
+++ src/pages/api/hatena.ts
@@ -0,0 +1,63 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import axios, { AxiosError } from 'axios';
+import { xmlToJson } from '../../utils/xmlToJson';
+
+const HATENA_ID = 'ryo_manba';
+const BLOG_ID = 'ryo-manba.hatenablog.com';
+const END_POINT = `https://blog.hatena.ne.jp/${HATENA_ID}/${BLOG_ID}/atom/entry`;
+
+interface Entry {
+  'app:control': [{ 'app:draft': string[] }];
+  published: string[];
+  title: string[];
+  link: { href: string[] }[];
+}
+
+interface HatenaPost {
+  day: string;
+  title: string;
+  href: string;
+}
+
+const getHatenaData = async () => {
+  try {
+    const response = await axios.get(END_POINT, {
+      auth: {
+        username: HATENA_ID,
+        password: process.env.HATENA_PASSWORD ?? '',
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const extractItemsAndNextUri = async (data: string) => {
+  const result = await xmlToJson(data);
+  const entry: Entry[] = result.feed.entry;
+  const nextUrl = result.feed.link[1].href;
+  return { entry, nextUrl };
+};
+
+const convertEntriesToItems = (entry: Entry[]): HatenaPost[] => {
+  return entry
+    .filter((e) => e['app:control'][0]['app:draft'][0] !== 'yes')
+    .map((e) => ({
+      day: e.published[0],
+      title: e.title[0],
+      href: e.link[1].href[0],
+    }));
+};
+
+export default async function hatena(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const xmlData = await getHatenaData();
+  const { entry } = await extractItemsAndNextUri(xmlData);
+  const hatenaPosts = convertEntriesToItems(entry);
+
+  res.status(200).json(hatenaPosts);
+}
