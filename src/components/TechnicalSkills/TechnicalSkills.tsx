4b06b1c7e34444506874519067e310c362175e8c
Ryo <matsukawa.ryo42@gmail.com>
2023-04-30 22:14:51 +0900

    add: image -> Imageに置き換え

--- src/components/TechnicalSkills/TechnicalSkills.tsx
+++ src/components/TechnicalSkills/TechnicalSkills.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 const languages = [
   'C',
   'C++',
@@ -24,14 +26,13 @@ const Skill = ({ imagePath, title, description }: SkillProps) => {
     <div className="skill-box">
       <div className="skill-title">
         <div className="img">
-          <img className="skill-icon" src={imagePath} />
-          {/* <Image
-        className="skill-icon"
-        src={imagePath}
-        width={100}
-        height={100}
-        alt={`${title}の画像`}
-      /> */}
+          <Image
+            className="skill-icon"
+            src={imagePath}
+            width={100}
+            height={100}
+            alt={`${title}の画像`}
+          />
         </div>
         <h3>{title}</h3>
       </div>
@@ -42,77 +43,81 @@ const Skill = ({ imagePath, title, description }: SkillProps) => {
 
 export const TechnicalSkills = () => {
   return (
-    <div className="inner">
-      <div className="header">
-        <h1>My Skills</h1>
-      </div>
-      <div className="container">
-        <Skill
-          title="HTML5"
-          imagePath="https://img.icons8.com/color/48/000000/html-5--v1.png"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+    <div className="skill-body">
+      <div className="inner">
+        <div className="header">
+          <h1 className="text-4xl font-semibold leading-none">
+            Skills & Abilities
+          </h1>
+        </div>
+        <div className="container">
+          <Skill
+            title="HTML5"
+            imagePath="https://img.icons8.com/color/48/000000/html-5--v1.png"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
                            commodo consequat."
-        />
-        <Skill
-          title="CSS3"
-          imagePath="https://img.icons8.com/color/48/000000/css3.png"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          />
+          <Skill
+            title="CSS3"
+            imagePath="https://img.icons8.com/color/48/000000/css3.png"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
                            commodo consequat."
-        />
-        <Skill
-          title="PostgreSQL"
-          imagePath="https://img.icons8.com/color/48/000000/postgreesql.png"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          />
+          <Skill
+            title="PostgreSQL"
+            imagePath="https://img.icons8.com/color/48/000000/postgreesql.png"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
                            commodo consequat."
-        />
-        <Skill
-          title="C++"
-          imagePath="https://img.icons8.com/fluency/48/null/c-plus-plus-logo.png"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          />
+          <Skill
+            title="C++"
+            imagePath="https://img.icons8.com/fluency/48/null/c-plus-plus-logo.png"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
                            commodo consequat."
-        />
-        <Skill
-          title="ReactJS"
-          imagePath="https://img.icons8.com/external-tal-revivo-color-tal-revivo/48/000000/external-react-a-javascript-library-for-building-user-interfaces-logo-color-tal-revivo.png"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          />
+          <Skill
+            title="ReactJS"
+            imagePath="https://img.icons8.com/external-tal-revivo-color-tal-revivo/48/000000/external-react-a-javascript-library-for-building-user-interfaces-logo-color-tal-revivo.png"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
                            commodo consequat."
-        />
-        <Skill
-          imagePath="/images/note-mark.png"
-          title="TypeScript"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          />
+          <Skill
+            imagePath="/images/note-mark.png"
+            title="TypeScript"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
                            commodo consequat."
-        />
-        <Skill
-          title="JavaScript"
-          imagePath="https://img.icons8.com/color/48/000000/javascript--v1.png"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          />
+          <Skill
+            title="JavaScript"
+            imagePath="https://img.icons8.com/color/48/000000/javascript--v1.png"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
                            commodo consequat."
-        />
-        <Skill
-          title="jQuery"
-          imagePath="https://img.icons8.com/ios-filled/48/1169ae/jquery.png"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          />
+          <Skill
+            title="jQuery"
+            imagePath="https://img.icons8.com/ios-filled/48/1169ae/jquery.png"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
                            commodo consequat."
-        />
-        <Skill
-          title="TailwindCSS"
-          imagePath="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/48px-Tailwind_CSS_Logo.png"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          />
+          <Skill
+            title="TailwindCSS"
+            imagePath="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/48px-Tailwind_CSS_Logo.png"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
                            commodo consequat."
-        />
-        <Skill
-          title="materialUI"
-          imagePath="https://img.icons8.com/color/48/000000/material-ui.png"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          />
+          <Skill
+            title="materialUI"
+            imagePath="https://img.icons8.com/color/48/000000/material-ui.png"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
                            commodo consequat."
-        />
-        <Skill
-          title="GitHub"
-          imagePath="https://img.icons8.com/glyph-neue/48/ffffff/github.png"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          />
+          <Skill
+            title="GitHub"
+            imagePath="https://img.icons8.com/glyph-neue/48/ffffff/github.png"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
                            commodo consequat."
-        />
+          />
+        </div>
       </div>
     </div>
   );
