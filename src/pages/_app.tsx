62ef2c858e7e60d68efabbc9c181a720e052fcb4
Ryo <matsukawa.ryo42@gmail.com>
2023-05-02 20:10:19 +0900

    fix: styleのpathを修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import '@/styles/globals.css';
+import '@/../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { BasicLayout } from '@/components/Layout/BasicLayout';
 import { NextPageWithLayout } from '@/types/NextPageWithLayout';
