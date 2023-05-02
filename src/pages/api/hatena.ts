3898c7cf34ab4ac61970b96fac322dae329bcc89
Ryo <matsukawa.ryo42@gmail.com>
2023-05-02 21:48:27 +0900

    modify: hatenaAPIの型定義を微調整した

--- src/pages/api/hatena.ts
+++ src/pages/api/hatena.ts
@@ -1,23 +1,23 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import axios, { AxiosError } from 'axios';
-import { xmlToJson } from '../../utils/xmlToJson';
+import axios from 'axios';
+import { xmlToJson } from '@/utils/xmlToJson';
 
 const HATENA_ID = 'ryo_manba';
 const BLOG_ID = 'ryo-manba.hatenablog.com';
 const END_POINT = `https://blog.hatena.ne.jp/${HATENA_ID}/${BLOG_ID}/atom/entry`;
 
-interface Entry {
+type Entry = {
   'app:control': [{ 'app:draft': string[] }];
   published: string[];
   title: string[];
   link: { href: string[] }[];
-}
+};
 
-interface HatenaPost {
+type HatenaPost = {
   day: string;
   title: string;
   href: string;
-}
+};
 
 const getHatenaData = async () => {
   try {
