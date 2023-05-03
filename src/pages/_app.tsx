8c9282f8aacdc1b7ca76d4d30bcf80c1439d2fd8
Ryo <matsukawa.ryo42@gmail.com>
2023-05-03 14:27:00 +0900

    add: faviconを設定した

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import '@/../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { BasicLayout } from '@/components/Layout/BasicLayout';
 import { NextPageWithLayout } from '@/types/NextPageWithLayout';
 
@@ -11,5 +12,29 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout =
     Component.getLayout ?? ((page) => <BasicLayout>{page}</BasicLayout>);
 
-  return getLayout(<Component {...pageProps} />);
+  return (
+    <>
+      <Head>
+        <link
+          rel="icon"
+          href="/favicon/icons8-kitty-cute-color-16.png"
+          sizes="16x16"
+          type="image/png"
+        />
+        <link
+          rel="icon"
+          href="/favicon/icons8-kitty-cute-color-32.png"
+          sizes="32x32"
+          type="image/png"
+        />
+        <link
+          rel="icon"
+          href="/favicon/icons8-kitty-cute-color-96.png"
+          sizes="96x96"
+          type="image/png"
+        />
+      </Head>
+      {getLayout(<Component {...pageProps} />)}
+    </>
+  );
 }
