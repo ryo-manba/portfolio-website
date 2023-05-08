673959c00dcfe0ba5801e2c741fb77c83bfff02c
Ryo <matsukawa.ryo42@gmail.com>
2023-05-09 08:37:34 +0900

    add: SNSアイコンにaria-labelを追加した

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,23 +10,33 @@ export const Footer = memo(function Footer() {
     <footer className="bg-[#D9D9D9] h-32 flex flex-col items-center justify-center">
       <div className="flex space-x-4 mb-2">
         <a href="https://github.com/ryo-manba" target="_blank" rel="noreferrer">
-          <TiSocialGithub className="text-4xl" />
+          <TiSocialGithub
+            className="text-4xl"
+            aria-label="Link: GitHubのプロフィール"
+          />
         </a>
         <a
           href="https://twitter.com/ryo_manba"
           target="_blank"
           rel="noreferrer"
         >
-          <TiSocialTwitter className="text-4xl" />
+          <TiSocialTwitter
+            className="text-4xl"
+            aria-label="Link: Twitterのプロフィール"
+          />
         </a>
         <a
           href="https://www.linkedin.com/in/ryo-matsukawa-ba5276214/"
           target="_blank"
           rel="noreferrer"
         >
-          <TiSocialLinkedin className="text-4xl" />
+          <TiSocialLinkedin
+            className="text-4xl"
+            aria-label="Link: LinkedInのプロフィール"
+          />
         </a>
       </div>
+
       <p className="text-sm">
         &copy; 2023{' '}
         <a
