c04976de6ae1a38790cb12f728ac49602d9b5747
Ryo Matsukawa <matsukawa.ryo42@gmail.com>
2023-06-04 12:16:05 +0900

    add: postsページにZennの記事が表示されるようにした

--- src/api/fetchRssFeeds.ts
+++ src/api/fetchRssFeeds.ts
@@ -2,16 +2,23 @@ import type { Post } from '@/types/post';
 import { fetchHatenaPosts } from '@/api/fetchHatenaPosts';
 import { fetchNotePosts } from '@/api/fetchNotePosts';
 import { fetchQiitaPosts } from '@/api/fetchQiitaPosts';
+import { fetchZennPosts } from '@/api/fetchZennPosts';
 
 export async function fetchRssFeeds(): Promise<Post[]> {
-  const [hatenaPosts, notePosts, qiitaPosts] = await Promise.all([
+  const [hatenaPosts, notePosts, qiitaPosts, zennPosts] = await Promise.all([
     fetchHatenaPosts(),
     fetchNotePosts(),
     fetchQiitaPosts(),
+    fetchZennPosts(),
   ]);
 
   // 新しい順に並び替える
-  const allPosts = [...hatenaPosts, ...notePosts, ...qiitaPosts].sort(
+  const allPosts = [
+    ...hatenaPosts,
+    ...notePosts,
+    ...qiitaPosts,
+    ...zennPosts,
+  ].sort(
     (a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime(),
   );
 
