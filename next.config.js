d558fa40172d6f3836343b103e45b576447ffe85
Ryo Matsukawa <76232929+ryo-manba@users.noreply.github.com>
2024-01-14 19:04:48 +0900

    add: package.json scripts

--- next.config.js
+++ next.config.js
@@ -1,14 +1,12 @@
 const withBundleAnalyzer =
-	process.env.ANALYZE === "true"
-		? require("@next/bundle-analyzer")({ enabled: true })
-		: (config) => config;
+  process.env.ANALYZE === "true" ? require("@next/bundle-analyzer")({ enabled: true }) : (config) => config;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	images: {
-		domains: ["img.icons8.com"],
-	},
+  reactStrictMode: true,
+  images: {
+    domains: ["img.icons8.com"],
+  },
 };
 
 module.exports = withBundleAnalyzer(nextConfig);
